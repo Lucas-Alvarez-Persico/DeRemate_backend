058f607571c7e46ff747e1e02fec3798421da7eb
Lucas-Alvarez-Persico <lucasalvarezpersico@gmail.com>
2025-03-15 10:47:08 -0300

    Add files via upload

---
+++ deRemate/src/main/java/com/deremate/demo/DeRemateApplication.java
@@ -0,0 +1,13 @@
+package com.deremate.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeRemateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeRemateApplication.class, args);
+	}
+
+}
