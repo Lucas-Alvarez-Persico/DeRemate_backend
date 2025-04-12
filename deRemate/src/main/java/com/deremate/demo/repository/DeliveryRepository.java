cd6b2c2031dea4b449242bb9aa2825f8a5ff8eb9
agusbarralia <agusbarralia@gmail.com>
2025-04-12 16:40:22 -0300

    Agregar @repository

--- deRemate/src/main/java/com/deremate/demo/repository/DeliveryRepository.java
+++ deRemate/src/main/java/com/deremate/demo/repository/DeliveryRepository.java
@@ -1,9 +1,10 @@
 package com.deremate.demo.repository;
-
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.deremate.demo.entity.Delivery;
 
+@Repository
 public interface DeliveryRepository extends JpaRepository<Delivery, Long> {
     
 }
