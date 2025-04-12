44e350de74f237a68c2d52f21942ce0c8dffe87d
agusbarralia <agusbarralia@gmail.com>
2025-04-12 16:42:50 -0300

    agregar @repository en delivery

--- deRemate/src/main/java/com/deremate/demo/repository/DeliveryRepository.java
+++ deRemate/src/main/java/com/deremate/demo/repository/DeliveryRepository.java
@@ -3,11 +3,14 @@ package com.deremate.demo.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.deremate.demo.entity.Delivery;
 import com.deremate.demo.entity.DeliveryStatus;
 import com.deremate.demo.entity.User;
 
+
+@Repository
 public interface DeliveryRepository extends JpaRepository<Delivery, Long> {
 
     List<Delivery> findByUserAndStatus(User user, DeliveryStatus status);
